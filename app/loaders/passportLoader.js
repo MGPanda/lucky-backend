54df43e6b4f245dce315fd17fd3cd00c5080f048
jesicavictora <jesicavictora@gmail.com>
2021-03-01 18:11:50 +0100

    inicializacion

---
+++ app/loaders/passportLoader.js
@@ -0,0 +1,12 @@
+const passport = require('passport');
+
+const localStrategies = require('./strategies/localStrategyLoader');
+
+function passportLoader(app) {
+
+    passport.use('login-local', localStrategies.loginStrategy);
+
+    app.use(passport.initialize());
+}
+
+module.exports = passportLoader;
---
+++ app/routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const passport = require('passport');
+
+const userController = require('./controllers/userController');
+const authController = require('./controllers/authController');
+const router = express.Router();
+
+router.route('/')
+    .get((req, res)=> {
+        res.json({ Mesaje: "welcome"});
+    });
+
+
+//LOGIN ROUTES//
+
+router.route('/login')
+    .post(passport.authenticate('login-local', { session: false }), authController.getToken);
+
+router.route('/register')
+    .post(userController.createUser);
+
+
+router.route('/logout')
+    .get(function(req, res){
+        req.logout();
+        res.redirect('/');
+      });
+
+
+router.route('/test')
+    .get(
+        authController.verifyToken, 
+        userController);
+//USER ROUTES//
+
+
+module.exports = router;
