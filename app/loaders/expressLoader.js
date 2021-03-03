215341c85cce850b8d7173c1a29e356fd13e6bb2
plex30 <elplanbee@gmail.com>
2021-03-03 12:18:39 +0100

    changes

--- app/loaders/expressLoader.js
+++ app/loaders/expressLoader.js
@@ -9,13 +9,19 @@ function expressLoader(app) {
     app.use(bodyParser.json());
 
     app.use(router);
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+      
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept, authorization"
+        );
+        res.header(
+          "Access-Control-Allow-Methods",
+          "PUT, GET, POST, DELETE, OPTIONS"
+        );
         next();
-    })
+      });
 
     app.use(function (req, res) {
         res.status(404).json({
